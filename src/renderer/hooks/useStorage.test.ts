966e32a73321bb58e2bfa59e5fa2d6c8a49dfebc
NoamR <noam.raby@gmail.com>
2022-09-06 13:37:22 +0200

    test hooks for both storage types

--- src/renderer/hooks/useStorage.test.ts
+++ src/renderer/hooks/useStorage.test.ts
@@ -1,5 +1,5 @@
-import { renderHook, act, cleanup } from "@testing-library/react";
-import { useSessionStorageItem } from "./useStorage";
+import { renderHook, act } from "@testing-library/react";
+import { useSessionStorageItem, useLocalStorageItem } from "./useStorage";
 
 describe("Test useStorage", () => {
   const myKey = "myKey";
@@ -24,82 +24,83 @@ describe("Test useStorage", () => {
     window.localStorage.clear();
     jest.clearAllMocks();
   });
+  [
+    { storage: localStorage, hook: useLocalStorageItem },
+    {
+      storage: sessionStorage,
+      hook: useSessionStorageItem,
+    },
+  ].forEach(({ storage, hook }) => {
+    describe(`Test ${hook.name}`, () => {
+      it(`${name} - should pass sanity`, () => {
+        expect(storage.getItem(myKey)).toBe(null);
+        const { result } = renderHook(() => hook<string>(myKey, stringValue));
 
-  it("should pass sanity", () => {
-    expect(window.sessionStorage.getItem(myKey)).toBe(null);
-    const { result } = renderHook(() =>
-      useSessionStorageItem<string>(myKey, stringValue),
-    );
+        const [value1, setValue] = result.current;
 
-    const [value1, setValue] = result.current;
+        expect(value1).toBe(stringValue);
+        expect(storage.getItem).toHaveBeenCalledWith(myKey);
+        expect(storage.setItem).toHaveBeenCalledWith(
+          myKey,
+          JSON.stringify(stringValue),
+        );
+        expect(JSON.parse(storage.getItem(myKey) as string)).toBe(stringValue);
 
-    expect(value1).toBe(stringValue);
-    expect(window.sessionStorage.getItem).toHaveBeenCalledWith(myKey);
-    expect(window.sessionStorage.setItem).toHaveBeenCalledWith(
-      myKey,
-      JSON.stringify(stringValue),
-    );
-    expect(JSON.parse(window.sessionStorage.getItem(myKey) as string)).toBe(
-      stringValue,
-    );
+        act(() => {
+          setValue(otherStringValue);
+        });
 
-    act(() => {
-      setValue(otherStringValue);
+        const [value2] = result.current;
+        expect(value2).toBe(otherStringValue);
+        expect(JSON.parse(storage.getItem(myKey) as string)).toBe(
+          otherStringValue,
+        );
+      });
+
+      it.each([
+        ["string", otherStringValue],
+        ["number", numberValue],
+        ["boolean", booleanValue],
+        ["array", arrayValue],
+        ["object", objectValue],
+      ])(`${name} - should work on input of type %s`, (_, valueUnderTest) => {
+        expect(storage.getItem(otherKey)).toStrictEqual(null);
+        const { result } = renderHook(() => hook(otherKey, valueUnderTest));
+
+        const [value] = result.current;
+
+        expect(value).toBe(valueUnderTest);
+        expect(JSON.parse(storage.getItem(otherKey) as string)).toStrictEqual(
+          valueUnderTest,
+        );
+      });
+
+      it("should be able to accept function as initial value", () => {
+        expect(storage.getItem(otherKey)).toBe(null);
+        const { result } = renderHook(() =>
+          hook<number>(myKey, () => numberValue),
+        );
+
+        const [value1, setValue] = result.current;
+
+        expect(value1).toBe(numberValue);
+
+        act(() => {
+          setValue(789);
+        });
+
+        const [value2] = result.current;
+        expect(value2).toBe(789);
+        expect(JSON.parse(storage.getItem(myKey) as string)).toBe(789);
+      });
+
+      it(`${name} - should read value from storage when init (hydrate)`, () => {
+        storage.setItem(myKey, JSON.stringify("bla"));
+        const { result } = renderHook(() => hook<number>(myKey));
+
+        const [value1] = result.current;
+        expect(value1).toBe("bla");
+      });
     });
-
-    const [value2] = result.current;
-    expect(value2).toBe(otherStringValue);
-    expect(JSON.parse(window.sessionStorage.getItem(myKey) as string)).toBe(
-      otherStringValue,
-    );
-  });
-
-  it.each([
-    ["string", otherStringValue],
-    ["number", numberValue],
-    ["boolean", booleanValue],
-    ["array", arrayValue],
-    ["object", objectValue],
-  ])("should work on input of type %s", (name, valueUnderTest) => {
-    expect(window.sessionStorage.getItem(otherKey)).toStrictEqual(null);
-    const { result } = renderHook(() =>
-      useSessionStorageItem(otherKey, valueUnderTest),
-    );
-
-    const [value] = result.current;
-
-    expect(value).toBe(valueUnderTest);
-    expect(
-      JSON.parse(window.sessionStorage.getItem(otherKey) as string),
-    ).toStrictEqual(valueUnderTest);
-  });
-
-  it("should be able to accept function as initial value", () => {
-    expect(window.sessionStorage.getItem(otherKey)).toBe(null);
-    const { result } = renderHook(() =>
-      useSessionStorageItem<number>(myKey, () => numberValue),
-    );
-
-    const [value1, setValue] = result.current;
-
-    expect(value1).toBe(numberValue);
-
-    act(() => {
-      setValue(789);
-    });
-
-    const [value2] = result.current;
-    expect(value2).toBe(789);
-    expect(JSON.parse(window.sessionStorage.getItem(myKey) as string)).toBe(
-      789,
-    );
-  });
-
-  it("should read value from storage when init (hydrate)", () => {
-    window.sessionStorage.setItem(myKey, JSON.stringify("bla"));
-    const { result } = renderHook(() => useSessionStorageItem<number>(myKey));
-
-    const [value1] = result.current;
-    expect(value1).toBe("bla");
   });
 });
